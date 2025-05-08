f80a289a3acc98005d3cb1d98ce681b1c21dbbf1
Balagovind-8 <rb.balagovind088@gmail.com>
2025-05-09 01:27:57 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Export auth, provider, and firestore
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider(); // <<== Add this line
 export const db = getFirestore(app);
